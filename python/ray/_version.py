e5fe89819e5ecd99cc9be382a0b15d33267bcd54
shiyanpeng03 <shiyanpeng03@kuaishou.com>
2026-07-09 22:16:50 +0800

    Bump ray wheel release version

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
-version = "2.55.1+kuaishou.{{RAY_COMMIT_SHA_SHORT}}"
+commit = "f0d7cc1f35b3422ed76082ff24602d2b63c2071c"
+version = "2.55.1+kuaishou.f0d7cc1f35"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))
